4ed464b5324ce31bee853523a37321a1d6d2dfec
SistemasTambini <sistemas@notariatambini.com>
2025-02-21 16:38:17 -0500

    ya

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/config');
 const libroReclamacionesRoutes = require('./src/routes/LibroRoutes');
-const emailRoutes = require('./src/routes/emailRoutes')
+const emailRoutes = require('./src/routes/emailRoutes');
 
 // Configurar dotenv
 dotenv.config();
@@ -12,8 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Configurar CORS para permitir solicitudes desde el frontend
+app.use(cors({
+    origin: '*', // Puedes restringirlo a 'http://127.0.0.1:5500' si lo deseas
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // Middlewares
-app.use(cors());
 app.use(express.json());
 
 // Conectar a MongoDB
@@ -25,21 +30,7 @@ app.use('/api/email', emailRoutes);
 
 // Ruta de prueba
 app.get('/', (req, res) => {
-    console.log('Alguien accedió a /');
     res.send('Hola Mundo');
 });
-
-// Rutas con logging
-app.use('/api/libroReclamaciones', (req, res, next) => {
-    console.log('📌 Alguien accedió a /api/libroReclamaciones');
-    next();
-}, libroReclamacionesRoutes);
-
-app.use('/api/email', (req, res, next) => {
-    console.log('📌 Alguien accedió a /api/email');
-    next();
-}, emailRoutes);
-
-
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
