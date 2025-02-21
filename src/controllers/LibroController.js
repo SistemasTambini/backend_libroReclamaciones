c5547ac5060315b1b8322034537c84f6008d93e3
SistemasTambini <sistemas@notariatambini.com>
2025-02-21 15:19:53 -0500

    ya

--- src/controllers/LibroController.js
+++ src/controllers/LibroController.js
@@ -4,13 +4,35 @@ const LibroReclamaciones = require('../models/LibroRModel');
 // Guardar un nuevo reclamo
 exports.guardarLibro = async (req, res) => {
     try {
-        const nuevoReclamo = new LibroReclamaciones(req.body);
+        // Mapear los nombres de los campos
+        const reclamoData = {
+            FechaRegistro: req.body.fechaRegistro || new Date(),
+            NombreCompleto: req.body.nombre,
+            Documento: req.body.dni,
+            Telefono: req.body.telefono,
+            Correo: req.body.correo,
+            Domicilio: req.body.domicilio,
+            Distrito: req.body.distrito,
+            Departamento: req.body.departamento,
+            Provincia: req.body.provincia,
+            Canal_Compra: req.body.canalCompra,
+            Producto: req.body.producto,
+            Costo: req.body.costoProducto,
+            TipoReclamo: req.body.tipoReclamo,
+            DetalleReclamo: req.body.detalle,
+            PedidoConsumidor: req.body.pedido,
+            Observacion: req.body.observaciones
+        };
+
+        const nuevoReclamo = new LibroReclamaciones(reclamoData);
         await nuevoReclamo.save();
+
         res.status(201).json({ message: 'Reclamo guardado exitosamente', data: nuevoReclamo });
     } catch (error) {
         res.status(500).json({ message: 'Error al guardar el reclamo', error });
     }
 };
+
 
 // Obtener el último ID registrado
 exports.obtenerUltimoID = async (req, res) => {
